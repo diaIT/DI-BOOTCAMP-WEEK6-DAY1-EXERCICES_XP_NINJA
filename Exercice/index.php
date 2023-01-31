661fd58985057fa82f35836e803a3b443b485ed2
Diallo boubacar <diallob456@gmail.com>
2023-01-31 14:27:55 +0100

    Exercice_XP_NINJA

---
+++ Exercice/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<title></title>
+</head>
+<body>
+<form action="valide.php" method="post">
+  <label for="fname"> Name:</label><br> 
+  <input type="text" id="name" name="name" value=""><br> <br>
+
+
+  <select id="" name="select">
+  	<option value="0">Yellow</option>
+  	<option value="1">Red</option>
+  	<option value="2">Black</option>
+  </select><br> <br>
+ 
+  <input type="submit" name="envoi">
+</form> 
+</body>
+</html>
---
+++ Exercice/valide.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<?php 
+if (isset($_POST['envoi'])) {
+	// code...
+	echo $_POST['name']; 
+
+	echo $_POST['select'];
+}
+
+ ?>
+</body>
+</html>
